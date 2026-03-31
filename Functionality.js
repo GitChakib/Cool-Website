4529573ed00424a3da32650e1429ccb42f6f097e
ChakibFehdSenoussi <faihasemon12345@gmail.com>
2026-03-31 21:06:35 +0100

    made a good example in the dashboard

--- Functionality.js
+++ Functionality.js
@@ -1,2 +1,3 @@
-document.getElementById("brief-description").textContent = `This is a brief description of the car. It provides an overview of the car's features, condition.`;
-document.getElementById("lcoation").textContent = `Location: New York, NY`;
+document.getElementsByClassName("brief-description")[5].textContent = `6-Speed Manual, Turbo V6, Passive Rear Wheel Steering, Mostly Unmodified`;
+document.getElementsByClassName("brief-description")[0].textContent = `Supercharged V6, QUATTRO AWD, Stock, 354-hp`;
+document.getElementsByClassName("location")[5].textContent = `New York, NY`;
