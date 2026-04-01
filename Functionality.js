a3f165df22e9b9a92b3ed61560e8288f1526d99a
ChakibFehdSenoussi <faihasemon12345@gmail.com>
2026-04-01 18:58:23 +0100

    broken version of dynamic listings

--- Functionality.js
+++ Functionality.js
@@ -12,14 +12,42 @@ function fetchresults(query) {
     console.log("search key:",query);
 }
 const carListings = [{
-    ispopular: false, transmission: 1, price: 35000, year: 2015, miles: 25000, make: "Audi", model: "S5", briefDescription: "Supercharged V6, QUATTRO AWD, Stock, 354-hp"
+    image: "Images/w48at.jpg", fullname: "2024 Mercedes-Benz AMG GLC 43", ispopular: false, transmission: 1, price: 68000, year: 2024, miles: 25000, make: "Mercedes-Benz", model: "GLC", briefDescription: "AWD, 2.0L I4 Turbo, Stock, 385-hp", location: "New York, NY"
 }, {
-    ispopular: false, transmission: 1, price: 54000, year: 2018, miles: 15000, make: "Porsche", model: "Panamera", briefDescription: "6-Speed Manual, Turbo V6, Passive Rear Wheel Steering, Mostly Unmodified"
+    image: "Images/X88vu.jpg", fullname: "2018 Chevrolet Camaro", ispopular: false, transmission: 1, price: 54000, year: 2018, miles: 15000, make: "Chevrolet", model: "Camaro", briefDescription: "6.2-Liter V8, Limited-Slip Differential, Mostly Unmodified", location: "Los Angeles, CA"
 }];
-carListings.forEach(car => {
-    console.log(car)
+
+const container = document.querySelector(".car-listing");
+
+carListings.forEach(carObj => {
+  const card = document.createElement("div");
+  card.className = "car";
+
+  const link = document.createElement("a");
+  link.href = "Car_Listing.html";
+
+  const img = document.createElement("img");
+  img.src = carObj.image;
+
+  link.appendChild(img);
+  card.appendChild(link);
+
+  const title = document.createElement("h3");
+  const titleLink = document.createElement("a");
+  titleLink.href = "Car_Listing.html";
+  titleLink.textContent = `${carObj.fullname}`;
+  title.appendChild(titleLink);
+  card.appendChild(title);
+
+  const brief = document.createElement("p");
+  brief.className = "brief-description";
+  brief.textContent = `${carObj.briefDescription}`;
+  card.appendChild(brief);
+
+  const location = document.createElement("p");
+  location.className = "location";
+  location.textContent = `${carObj.location}`;
+  card.appendChild(location);
+
+  container.appendChild(card);
 });
-console.log(carListings);
-document.getElementsByClassName("brief-description")[5].textContent = `6-Speed Manual, Turbo V6, Passive Rear Wheel Steering, Mostly Unmodified`;
-document.getElementsByClassName("brief-description")[0].textContent = `Supercharged V6, QUATTRO AWD, Stock, 354-hp`;
-document.getElementsByClassName("location")[5].textContent = `New York, NY`;
