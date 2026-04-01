ac05697f67e6a9a334b98cff06e28a1ef2b35f86
ChakibFehdSenoussi <faihasemon12345@gmail.com>
2026-04-01 16:52:25 +0100

    started making it dynamic

--- Functionality.js
+++ Functionality.js
@@ -11,6 +11,15 @@ searchInput.addEventListener("keydown", function(event){
 function fetchresults(query) {
     console.log("search key:",query);
 }
+const carListings = [{
+    ispopular: false, transmission: 1, price: 35000, year: 2015, miles: 25000, make: "Audi", model: "S5", briefDescription: "Supercharged V6, QUATTRO AWD, Stock, 354-hp"
+}, {
+    ispopular: false, transmission: 1, price: 54000, year: 2018, miles: 15000, make: "Porsche", model: "Panamera", briefDescription: "6-Speed Manual, Turbo V6, Passive Rear Wheel Steering, Mostly Unmodified"
+}];
+carListings.forEach(car => {
+    console.log(car)
+});
+console.log(carListings);
 document.getElementsByClassName("brief-description")[5].textContent = `6-Speed Manual, Turbo V6, Passive Rear Wheel Steering, Mostly Unmodified`;
 document.getElementsByClassName("brief-description")[0].textContent = `Supercharged V6, QUATTRO AWD, Stock, 354-hp`;
 document.getElementsByClassName("location")[5].textContent = `New York, NY`;
